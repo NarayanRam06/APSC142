a38b79ceb912013ea8a04a13f10cd1a5c6409767
NarayanRam06 <06narayanram@gmail.com>
2025-01-14 18:54:44 -0500

    Taylor Series function implemented

--- Assignment1/apsc142assign1.c
+++ Assignment1/apsc142assign1.c
@@ -13,8 +13,8 @@ double integralTaylorSeries(double lowerLimit, double upperLimit, int numTerms);
 double riemannSum(double lowerLimit, double upperLimit, int numRectangles);
 
 int main(void) {
-    printf("%f",inverseFactorial(0));
-
+    printf("%f\n",inverseFactorial(0));
+    printf("%f\n", taylorSeries(-3,100));
 }
 
 //Uses Equation (1) to evaluate the inverse factorial of an input integer
@@ -26,3 +26,14 @@ double inverseFactorial(int n) {
     }
     return invFact;
 }
+//Uses Equation (3) to evaluate an approximation of e^x^2.
+//Takes in value of x as a double, and number of terms in taylor series as integer
+double taylorSeries(double x, int numTerms) {
+    double num = 0;
+    for (int i = 0; i < numTerms -1;i++) {
+        num+= inverseFactorial(i)* pow(x,2*i);
+    }
+    return num;
+
+}
+

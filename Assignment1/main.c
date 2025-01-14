e3380daf980aa23f8562af33130e5803198f4b78
NarayanRam06 <06narayanram@gmail.com>
2025-01-13 20:10:35 -0500

    Inverse Factorial Function update 1

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -10,14 +10,18 @@ double inverseFactorial(int n);
 double taylorSeries(double x, int numTerms);
 double integralTaylorSeries(double lowerLimit, double upperLimit, int numTerms);
 double riemannSum(double lowerLimit, double upperLimit, int numRectangles);
+
 int main(void) {
-    printf("%f",inverseFactorial(3));
+    printf("%f",inverseFactorial(0));
     return 0;
 }
+
+//Uses Equation (1) to evaluate the inverse factorial of an input integer
 double inverseFactorial(int n) {
     double invFact = 1;
+    //loop: divide 1 by n until n = 0
     for (int i = n;i>0;i--) {
-        invFact*=(1.0/i);
+        invFact/=i;
     }
     return invFact;
 }

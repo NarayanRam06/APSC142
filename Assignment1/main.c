0227c52dbdf9150e58fff90d3106f4522964df24
NarayanRam06 <06narayanram@gmail.com>
2025-01-13 19:44:12 -0500

    Test Update

--- Assignment1/main.c
+++ Assignment1/main.c
@@ -2,6 +2,14 @@
 /*
  * Use C to calculate complex mathematical expressions.
  */
+
+
+
+//Declaring Functions
+double inverseFactorial(int n);
+double taylorSeries(double x, int numTerms);
+double integralTaylorSeries(double lowerLimit, double upperLimit, int numTerms);
+double riemannSum(double lowerLimit, double upperLimit, int numRectangles);
 int main(void) {
     printf("Hello, World!\n");
     return 0;
